3ce5ad438db9a439c7035f1a3d49602c0e15dc0b
Isaiah <52585456+Isaiahdj@users.noreply.github.com>
2021-10-26 16:40:53 -0700

    methodClass

---
+++ methodClass/Math.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodClass
+{
+    public class Math
+    {
+
+        public static int num;
+        public static void Squared()
+        {
+            long square = num * num;
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write("Your number squared = ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(square);
+            Console.WriteLine();
+        }
+
+        public static void Half()
+        {
+            double half = (double)num / 2;
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write("Your number cut in half = ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(half);
+            Console.WriteLine();
+        }
+
+        public static void Doubled()
+        {
+            long doubled = num * 2;
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write("Your number doubled = ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(doubled);
+        }
+
+    }
+}
---
+++ methodClass/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Im going to ask you for a number then i will do some math on that number and print it out to you");
+        number:
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.Write("please enter in a number:     ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Math.num = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (OverflowException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Your number is way to big please enter a different number");
+                goto number;
+            }
+            catch (FormatException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please enter a whole number");
+                goto number;
+            }
+            Console.WriteLine();
+            Math.Squared();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Press enter to do more math on your number");
+            Console.ReadLine();
+            Math.Half();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Press enter to do math on your number one last time");
+            Console.ReadLine();
+            Math.Doubled();
+            Console.ReadLine();
+        }
+    }
+}
