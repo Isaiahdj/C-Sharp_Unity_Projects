e1df2b50e4da53b01c0e4df1e509aea2650f8d25
Isaiah <52585456+Isaiahdj@users.noreply.github.com>
2021-10-28 15:36:54 -0700

    inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee() { FirstName = "Sample", LastName = "Student" };
+            person.SayName();
+            Console.ReadLine();
+        }
+    }
+}
