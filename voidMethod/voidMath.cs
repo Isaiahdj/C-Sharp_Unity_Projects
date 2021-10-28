e6b81d69a8f3d5a4b01decc2bdbe26398e60aee7
Isaiah <52585456+Isaiahdj@users.noreply.github.com>
2021-10-27 17:01:43 -0700

    voidMethod

---
+++ voidMethod/voidMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace voidMethod
+{
+    class voidMath
+    {
+        public void vMath(int val1, int val2)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nThe first value {0} multiplied by 3 is {1}", val1, val1 * 3);
+            Console.WriteLine("\nThe second value is {0}", val2);
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
