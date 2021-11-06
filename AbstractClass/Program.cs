c6240e7d17668b0f99cb51c036e7ed0108ef3422
Isaiah <52585456+Isaiahdj@users.noreply.github.com>
2021-11-06 16:13:40 -0700

    AbstractClass

---
+++ AbstractClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create object 
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            //call method
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
