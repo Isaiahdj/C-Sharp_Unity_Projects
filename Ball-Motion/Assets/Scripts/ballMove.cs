076cae39867e71f227359799c1953155c497e080
Isaiah <52585456+Isaiahdj@users.noreply.github.com>
2021-12-31 15:46:52 -0800

    Ball-motion
    A ball maze with a main menu

---
+++ Ball-Motion/Assets/Scripts/ballMove.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class ballMove : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody rigb;
+    void Start()
+    {
+        rigb = GetComponent<Rigidbody>();
+
+    }
+    void FixedUpdate()
+    {
+        float moveHoriz = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHoriz, 0.0f, moveVert);
+        rigb.AddForce(movement * speed);
+    }
+}
